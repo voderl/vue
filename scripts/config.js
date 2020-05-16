810de37c6c2404b45343ca814decc4189c6a8e97
voderl <voderl@qq.com>
2020-05-16 22:50:12 +0800

    update output config

--- scripts/config.js
+++ scripts/config.js
@@ -1,39 +1,40 @@
-const path = require('path')
-const buble = require('rollup-plugin-buble')
-const alias = require('rollup-plugin-alias')
-const cjs = require('rollup-plugin-commonjs')
-const replace = require('rollup-plugin-replace')
-const node = require('rollup-plugin-node-resolve')
-const flow = require('rollup-plugin-flow-no-whitespace')
-const version = process.env.VERSION || require('../package.json').version
-const weexVersion = process.env.WEEX_VERSION || require('../packages/weex-vue-framework/package.json').version
-const featureFlags = require('./feature-flags')
+const path = require('path');
+const buble = require('rollup-plugin-buble');
+const alias = require('rollup-plugin-alias');
+const cjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
+const node = require('rollup-plugin-node-resolve');
+const flow = require('rollup-plugin-flow-no-whitespace');
+const version = process.env.VERSION || require('../package.json').version;
+const weexVersion =
+  process.env.WEEX_VERSION || require('../packages/weex-vue-framework/package.json').version;
+const featureFlags = require('./feature-flags');
 
 const banner =
   '/*!\n' +
   ` * Vue.js v${version}\n` +
   ` * (c) 2014-${new Date().getFullYear()} Evan You\n` +
   ' * Released under the MIT License.\n' +
-  ' */'
+  ' */';
 
 const weexFactoryPlugin = {
-  intro () {
-    return 'module.exports = function weexFactory (exports, document) {'
+  intro() {
+    return 'module.exports = function weexFactory (exports, document) {';
   },
-  outro () {
-    return '}'
-  }
-}
+  outro() {
+    return '}';
+  },
+};
 
-const aliases = require('./alias')
-const resolve = p => {
-  const base = p.split('/')[0]
+const aliases = require('./alias');
+const resolve = (p) => {
+  const base = p.split('/')[0];
   if (aliases[base]) {
-    return path.resolve(aliases[base], p.slice(base.length + 1))
+    return path.resolve(aliases[base], p.slice(base.length + 1));
   } else {
-    return path.resolve(__dirname, '../', p)
+    return path.resolve(__dirname, '../', p);
   }
-}
+};
 
 const builds = {
   // Runtime only (CommonJS). Used by bundlers e.g. Webpack & Browserify
@@ -42,14 +43,14 @@ const builds = {
     dest: resolve('dist/vue.runtime.common.dev.js'),
     format: 'cjs',
     env: 'development',
-    banner
+    banner,
   },
   'web-runtime-cjs-prod': {
     entry: resolve('web/entry-runtime.js'),
     dest: resolve('dist/vue.runtime.common.prod.js'),
     format: 'cjs',
     env: 'production',
-    banner
+    banner,
   },
   // Runtime+compiler CommonJS build (CommonJS)
   'web-full-cjs-dev': {
@@ -58,7 +59,7 @@ const builds = {
     format: 'cjs',
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   'web-full-cjs-prod': {
     entry: resolve('web/entry-runtime-with-compiler.js'),
@@ -66,14 +67,14 @@ const builds = {
     format: 'cjs',
     env: 'production',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime only ES modules build (for bundlers)
   'web-runtime-esm': {
     entry: resolve('web/entry-runtime.js'),
     dest: resolve('dist/vue.runtime.esm.js'),
     format: 'es',
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for bundlers)
   'web-full-esm': {
@@ -81,7 +82,7 @@ const builds = {
     dest: resolve('dist/vue.esm.js'),
     format: 'es',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for direct import in browser)
   'web-full-esm-browser-dev': {
@@ -91,7 +92,7 @@ const builds = {
     transpile: false,
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler ES modules build (for direct import in browser)
   'web-full-esm-browser-prod': {
@@ -101,7 +102,7 @@ const builds = {
     transpile: false,
     env: 'production',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // runtime-only build (Browser)
   'web-runtime-dev': {
@@ -109,7 +110,7 @@ const builds = {
     dest: resolve('dist/vue.runtime.js'),
     format: 'umd',
     env: 'development',
-    banner
+    banner,
   },
   // runtime-only production build (Browser)
   'web-runtime-prod': {
@@ -117,32 +118,34 @@ const builds = {
     dest: resolve('dist/vue.runtime.min.js'),
     format: 'umd',
     env: 'production',
-    banner
+    banner,
   },
   // Runtime+compiler development build (Browser)
   'web-full-dev': {
     entry: resolve('web/entry-runtime-with-compiler.js'),
-    dest: resolve('dist/vue.js'),
+    dest: resolve('template/libs/vue.js'),
     format: 'umd',
+    external: ['pixi.js-legacy'],
     env: 'development',
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Runtime+compiler production build  (Browser)
   'web-full-prod': {
     entry: resolve('web/entry-runtime-with-compiler.js'),
-    dest: resolve('dist/vue.min.js'),
+    dest: resolve('template/libs/vue.min.js'),
     format: 'umd',
     env: 'production',
+    external: ['pixi.js-legacy'],
     alias: { he: './entity-decoder' },
-    banner
+    banner,
   },
   // Web compiler (CommonJS).
   'web-compiler': {
     entry: resolve('web/entry-compiler.js'),
     dest: resolve('packages/vue-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-template-compiler/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-template-compiler/package.json').dependencies),
   },
   // Web compiler (UMD for in-browser use).
   'web-compiler-browser': {
@@ -151,7 +154,7 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'VueTemplateCompiler',
-    plugins: [node(), cjs()]
+    plugins: [node(), cjs()],
   },
   // Web server renderer (CommonJS).
   'web-server-renderer-dev': {
@@ -159,14 +162,14 @@ const builds = {
     dest: resolve('packages/vue-server-renderer/build.dev.js'),
     format: 'cjs',
     env: 'development',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-prod': {
     entry: resolve('web/entry-server-renderer.js'),
     dest: resolve('packages/vue-server-renderer/build.prod.js'),
     format: 'cjs',
     env: 'production',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-basic': {
     entry: resolve('web/entry-server-basic-renderer.js'),
@@ -174,19 +177,19 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'renderVueComponentToString',
-    plugins: [node(), cjs()]
+    plugins: [node(), cjs()],
   },
   'web-server-renderer-webpack-server-plugin': {
     entry: resolve('server/webpack-plugin/server.js'),
     dest: resolve('packages/vue-server-renderer/server-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   'web-server-renderer-webpack-client-plugin': {
     entry: resolve('server/webpack-plugin/client.js'),
     dest: resolve('packages/vue-server-renderer/client-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies),
   },
   // Weex runtime factory
   'weex-factory': {
@@ -194,14 +197,14 @@ const builds = {
     entry: resolve('weex/entry-runtime-factory.js'),
     dest: resolve('packages/weex-vue-framework/factory.js'),
     format: 'cjs',
-    plugins: [weexFactoryPlugin]
+    plugins: [weexFactoryPlugin],
   },
   // Weex runtime framework (CommonJS).
   'weex-framework': {
     weex: true,
     entry: resolve('weex/entry-framework.js'),
     dest: resolve('packages/weex-vue-framework/index.js'),
-    format: 'cjs'
+    format: 'cjs',
   },
   // Weex compiler (CommonJS). Used by Weex's Webpack loader.
   'weex-compiler': {
@@ -209,63 +212,63 @@ const builds = {
     entry: resolve('weex/entry-compiler.js'),
     dest: resolve('packages/weex-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/weex-template-compiler/package.json').dependencies)
-  }
-}
+    external: Object.keys(require('../packages/weex-template-compiler/package.json').dependencies),
+  },
+};
 
-function genConfig (name) {
-  const opts = builds[name]
+function genConfig(name) {
+  const opts = builds[name];
   const config = {
     input: opts.entry,
     external: opts.external,
-    plugins: [
-      flow(),
-      alias(Object.assign({}, aliases, opts.alias))
-    ].concat(opts.plugins || []),
+    plugins: [flow(), alias(Object.assign({}, aliases, opts.alias))].concat(opts.plugins || []),
     output: {
       file: opts.dest,
       format: opts.format,
       banner: opts.banner,
-      name: opts.moduleName || 'Vue'
+      name: opts.moduleName || 'Vue',
+      globals: {
+        'pixi.js-legacy': 'PIXI',
+      },
     },
     onwarn: (msg, warn) => {
       if (!/Circular/.test(msg)) {
-        warn(msg)
+        warn(msg);
       }
-    }
-  }
+    },
+  };
 
   // built-in vars
   const vars = {
     __WEEX__: !!opts.weex,
     __WEEX_VERSION__: weexVersion,
-    __VERSION__: version
-  }
+    __VERSION__: version,
+  };
   // feature flags
-  Object.keys(featureFlags).forEach(key => {
-    vars[`process.env.${key}`] = featureFlags[key]
-  })
+  Object.keys(featureFlags).forEach((key) => {
+    vars[`process.env.${key}`] = featureFlags[key];
+  });
   // build-specific env
   if (opts.env) {
-    vars['process.env.NODE_ENV'] = JSON.stringify(opts.env)
+    vars['process.env.NODE_ENV'] = JSON.stringify(opts.env);
   }
-  config.plugins.push(replace(vars))
+  config.plugins.push(replace(vars));
 
   if (opts.transpile !== false) {
-    config.plugins.push(buble())
+    config.plugins.push(buble());
   }
 
   Object.defineProperty(config, '_name', {
     enumerable: false,
-    value: name
-  })
+    value: name,
+  });
 
-  return config
+  return config;
 }
 
 if (process.env.TARGET) {
-  module.exports = genConfig(process.env.TARGET)
+  module.exports = genConfig(process.env.TARGET);
 } else {
-  exports.getBuild = genConfig
-  exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+  exports.getBuild = genConfig;
+  exports.getAllBuilds = () => Object.keys(builds).map(genConfig);
 }
